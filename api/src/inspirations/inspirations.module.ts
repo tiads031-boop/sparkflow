8e3f19929bf63178e2e29f01787736e6b6a25c1c
tiads031-boop <tiads031@gmail.com>
2026-09-20 03:47:09 +0800

    feat(inspirations): wire shared audio transcription

--- api/src/inspirations/inspirations.module.ts
+++ api/src/inspirations/inspirations.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { InspirationsController } from './inspirations.controller';
 import { InspirationsService } from './inspirations.service';
 import { InspirationMediaService } from './inspiration-media.service';
+import { AiModule } from '../ai/ai.module';
 
 @Module({
+  imports: [AiModule],
   controllers: [InspirationsController],
   providers: [InspirationsService, InspirationMediaService],
 })
